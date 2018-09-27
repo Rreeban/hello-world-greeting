2cbf6dc805ea522eff22852fb764ddfdb5bdebfc
Rreeban <36625657+Rreeban@users.noreply.github.com>
2018-09-27 12:04:17 +0200

    Update GreetingMessageIT.java

--- src/test/java/GreetingMessageIT.java
+++ src/test/java/GreetingMessageIT.java
@@ -18,11 +18,11 @@ public class GreetingMessageIT {
 	if (hour < 12)
     this.message = "Good Morning?";
     else if (hour < 17 && !(hour == 12))
-    this.message = "Good Afternoon!";
+    this.message = "Good Afternoon?";
     else if (hour == 12)
-    this.message = "Good Noon!";
+    this.message = "Good Noon?";
     else
-    this.message = "Good Evening!";
+    this.message = "Good Evening?";
    	
     assertEquals(message,var.printMessage());
 
